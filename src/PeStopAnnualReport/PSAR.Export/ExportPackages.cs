83198172200f70ae7d054b3d43a2db844a79caef
Andrei Ignat <ignatandrei@yahoo.com>
2026-01-31 23:09:39 +0200

    start adding export

---
+++ src/PeStopAnnualReport/PSAR.Export/ExportPackages.cs
@@ -0,0 +1,18 @@
+﻿using PSARModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace PSAR.Export;
+
+public record Display(ValidationResult[] problems)
+public class ExportPackages
+{
+    public async Task<Stream> Export(PackagesList packages)
+    {
+        var validProblems = packages.Validate(new ValidationContext(this)).ToArray();
+
+        using var ms = new MemoryStream();
+
+        return ms;
+    }
+
+}
